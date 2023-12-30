47b70f887980532da49882f83f0f5622df6785da
achibulup <achibulup@gmail.com>
2023-12-31 02:52:52 +0700

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
+import LoginForm from '../ui/login/login-form'
+import { getUserProfile } from '@/lib/backend/auth/session'
+import { redirect } from 'next/navigation'
 
-export default function page() {
-  // TODO: stub
+export default async function page() {
+  const session = await getUserProfile();
+  if (session) {
+    redirect('/home');
+  }
   return (
-    <div>login
-      <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <Link href="/dashboard">Login</Link>
-      </form>
-    </div>
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            MagicPost logo
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
   )
 }
